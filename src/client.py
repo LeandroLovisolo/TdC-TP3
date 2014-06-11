be573da9f8e79ee5a781a1041c173cdc82cf264a
Leandro Lovisolo <leandro@leandro.me>
2014-06-11 05:33:57 -0300

    Cliente y servidor parametrizados

--- src/client.py
+++ src/client.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python2
 # coding: utf-8
 
+import argparse
 from ptc import Socket, SHUT_WR
 from struct import pack
 from time import clock
+from socket import gethostbyname
 
-msg = 'a' * 1000000
+def main(server_ip, port, size, ack_delay, loss_probability):
+    msg = 'a' * size
 
-with Socket() as sock:
-	sock.connect(('127.0.0.1', 6677))
-	print 'is connected: ' + str(sock.is_connected())
+    with Socket(ack_delay=ack_delay,
+                loss_probability=loss_probability) as sock:
+      sock.connect((server_ip, port))
+      print 'connection established'
 
-	print 'sending file size: %d...' % len(msg)
-	sock.send(pack('I', len(msg)))
+      print 'sending file size...'
+      sock.send(pack('I', size))
 
-	print 'sending file...'
-	t0 = clock()
-	sock.send(msg)
-	t1 = clock()
+      print 'sending %d bytes...' % size
+      t0 = clock()
+      sock.send(msg)
+      t1 = clock()
 
-	t = t1 - t0
-	print 'took %f seconds' % t
+      t = t1 - t0
+      print 'took %f seconds' % t
 
-	# Cerramos el stream de escritura pero podemos seguir recibiendo datos.
-	sock.shutdown(SHUT_WR)
-print 'all done'
+      # Cerramos el stream de escritura pero podemos seguir recibiendo datos.
+      sock.shutdown(SHUT_WR)
+    print 'all done'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('hostname', help='server hostname')
+    parser.add_argument('port', type=int, help='server PTC port')
+    parser.add_argument('size', type=int, help='number of bytes to be sent')
+    parser.add_argument('-d', '--delay', type=float, default=0.0,
+                        help='ACK delay in seconds (default 0)')
+    parser.add_argument('-l', '--loss', type=float, default=0.0,
+                        help='probability of losing a packet (default 0)')
+    args = parser.parse_args()
+
+    main(gethostbyname(args.hostname), args.port, args.size, args.delay, args.loss)
--- src/server.py
+++ src/server.py
@@ -1,33 +1,46 @@
 #!/usr/bin/env python2
 # coding: utf-8
 
+import argparse
 from ptc import Socket
 from struct import unpack
 
-received = str()
+def main(port, ack_delay, loss_probability):
+    received = str()
 
-with Socket() as sock:
-    sock.bind(('127.0.0.1', 6677))
-    sock.listen()
+    while True:
+        with Socket(ack_delay=ack_delay,
+                    loss_probability=loss_probability) as sock:
+            sock.bind(('0.0.0.0', port))
+            sock.listen()
 
-    print 'waiting for a client...'
-    
-    sock.accept()
+            print 'waiting for a client...'
+            
+            sock.accept()
 
-    print 'client connected'
+            print 'connection established'
 
-    size_str = sock.recv(4)
-    size = unpack('I', size_str)[0]
+            size_str = sock.recv(4)
+            size = unpack('I', size_str)[0]
 
-    print 'receiving %d bytes...' % size
+            print 'receiving %d bytes...' % size
 
-    while len(received) < size:
-    	received += sock.recv(size - len(received))
+            while len(received) < size:
+                received += sock.recv(size - len(received))
 
-    print 'closing socket...'
-    
-    sock.close()
+            print 'closing socket...'
 
-#print 'received: %s' % received
+            sock.close()
+        #print 'received: %s' % received
+        print 'disconnected\n'
 
-print 'all done'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', type=int, help='port to listen to')
+    parser.add_argument('-d', '--delay', type=float, default=0.0,
+                        help='ACK delay in seconds (default 0)')
+    parser.add_argument('-l', '--loss', type=float, default=0.0,
+                        help='probability of losing a packet (default 0)')
+    args = parser.parse_args()
+
+    main(args.port, args.delay, args.loss)
