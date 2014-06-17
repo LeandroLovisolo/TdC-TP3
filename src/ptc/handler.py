5d2a1aa0705c9e003e3c9bf09860914f76bec79e
Leandro Lovisolo <leandro@leandro.me>
2014-06-17 18:26:04 -0300

    Logging

--- src/ptc/handler.py
+++ src/ptc/handler.py
@@ -142,6 +142,7 @@ class IncomingPacketHandler(object):
         if FINFlag in packet:
             self.handle_incoming_fin(packet, next_state=CLOSE_WAIT)
         else:
+            self.logger.debug('Paquete recibido: window=%d' % packet.get_window_size())
             self.process_on_control_block(packet)
             if not self.control_block.has_data_to_send():
                 # Si hay datos a punto de enviarse, "piggybackear" el ACK ahí
