6c09681ba8e2b5fdf0be0fee49c0edb90a42be83
Leandro Lovisolo <leandro@leandro.me>
2014-06-19 19:10:42 -0300

    Leyenda

--- src/time_vs_delay_plot.py
+++ src/time_vs_delay_plot.py
@@ -6,12 +6,11 @@ import plot
 class TimeVsDelayPlot(plot.Plot):
 
     def do_plot(self, plt, fig, db):
-        colors = ['Green', 'DarkCyan', 'Indigo', 'Brown', 'DarkGoldenRod', 'Crimson', 'Orange', 'GreenYellow', 'DarkMagenta', 'MidnightBlue', 'DimGray']
+        colors = ['Green', 'DarkCyan', 'Indigo', 'Brown', 'DarkGoldenRod', 'Crimson',
+                  'Orange', 'GreenYellow', 'DarkMagenta', 'MidnightBlue', 'DimGray']
         loss_probabilities = db.get_time_vs_delay_loss_probabilities()
 
-        for i in range(11):
-            loss = loss_probabilities[i]
-
+        for loss in loss_probabilities:
             time_vs_delay = db.get_time_vs_delay(loss)
 
             data = time_vs_delay.items()
@@ -20,13 +19,14 @@ class TimeVsDelayPlot(plot.Plot):
             delays = [s * 1000 for s in data[0]]
             avgs = list(data[1])
 
-            plt.plot(delays, avgs, color=colors[i])
+            plt.plot(delays, avgs, color=colors.pop(), label=str(loss))
 
         plt.xticks(delays, delays, fontsize=9, rotation=90)
         plt.xlim([delays[0], delays[-1]])
         plt.title(u'Tiempo de transferencia en función del retraso en envío de ACKs')
         plt.xlabel(u'Retraso en envío de ACKs (ms)')
         plt.ylabel(u'Tiempo de transferencia promedio (s)')
+        plt.legend(loc=2)
 
 if __name__ == '__main__':
     TimeVsDelayPlot()
