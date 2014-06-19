1f2392be1e1d7288a8406dbc8bfd56bf414cf692
Nahuel <nahueldelgado@gmail.com>
2014-06-18 23:37:46 -0300

    Agregada la clase para plotear el experimento 2. TODO: Completarla.

---
+++ src/time_vs_delay_plot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import plot
+
+class TimeVsDelayPlot(plot.Plot):
+
+    def do_plot(self, plt, fig, db):
+        pass
+
+if __name__ == '__main__':
+    TimeVsDelayPlot()
